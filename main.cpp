08a7bb893f3dbfd361627d8101d88b0469f3fac2
Haggy93 <zielak1993@interia.pl>
2016-12-17 10:48:41 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "main.hpp"
 
-#define VERSION 1.4
+#define VERSION 1.5
 
 int main()
 {
 	cout<<"Version : "<<VERSION<<endl;
+	
 	GSMinit();
+	
 	int lcd = wiringPiI2CSetup(LCD_Address);
 	if (lcd == -1)   
     {
@@ -14,21 +16,20 @@ int main()
     else
     {
 		LCDinit(lcd);
+		LCDclear(lcd, 0);
+		LCDsetCursor(lcd, 0, 1);
+		LCDwrite(lcd, "VERSION 1.5");
 		while(1)
 		{
 			if(GSMhowManySMS() != 0)
 			{
 				LCDclear(lcd, 0);
 				LCDsetCursor(lcd, 0, 1);
-				LCDwrite(lcd, GSMgetDate(GSMreadSMS(1)));
+				LCDwrite(lcd, GSMgetNumber(GSMreadSMS(1)));
 				LCDsetCursor(lcd, 0, 2);
 				LCDwrite(lcd, GSMgetMessage(GSMreadSMS(1)));
 				deleteAllSMS();
 			}
 		}
 	}
-	
-	//string numer = "+48723320133";
-	//string wiadomosc = "*****";
-	//sendSMS(numer, wiadomosc);
 }
