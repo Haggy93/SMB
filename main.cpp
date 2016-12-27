c383e049a8497e352d9554e8ffde2c696d64d077
Haggy93 <zielak1993@interia.pl>
2016-12-27 21:58:13 +0100

    12

--- main.cpp
+++ main.cpp
@@ -3,27 +3,28 @@
 #define VERSION 1.4
 
 static const int SPIspeed = 500000; /* Hz */
-static const int SPIchannel = 0;
+static const int SPIchannel = 1;
 static const unsigned char SPIdataSize = 4;
 
 int main()
 {
-	wiringPiSetupPhys();
 	cout<<"Version : "<<VERSION<<endl;
 	cout<<"Program : SPI test" <<endl;
 	cout<<"SPI init result : " << wiringPiSPISetup(SPIchannel, SPIspeed)<<endl;
 			
 	//unsigned char SPIdata[SPIdataSize]= {0x00, 0x10, 0x20, 0xff};
 	
-	uint8_t Mydata = 0xff;
+	unsigned char myData[100];
 	
 	cout<<"Start work..."<<endl;
+	
+	myData[0] = 0x76;
 	
 	int SPIresult = 0;
 	while(1)
 	{
 		delay(500);
-		SPIresult = wiringPiSPIDataRW(SPIchannel, &Mydata , 1);
+		SPIresult = wiringPiSPIDataRW(SPIchannel, myData , 1);
 		cout<<"Write result : "<< SPIresult <<endl;
 	}
 }
